a69d608a76a2d2bdadbcecec34b31ffb748e3b54
Khatchi <solomon9ice2014@gmail.com>
2025-05-16 09:39:46 +0100

    Aggregate and Window functions modified

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -25,6 +25,47 @@ GROUP BY
 -- Using window function (ROW_NUMBER, RANK) to rank properties based 
 -- on the total number of bookings they have received.
 
+-- WITH BookingCounts AS (
+--     SELECT 
+--         p.property_id,
+--         p.host_id,
+--         p.name,
+--         p.description,
+--         p.location,
+--         p.pricepernight,
+--         p.created_at,
+--         p.updated_at,
+--         COUNT(b.booking_id) AS total_bookings
+--     FROM 
+--         Property p
+--     LEFT JOIN 
+--         Booking b
+--     ON 
+--         p.property_id = b.property_id
+--     GROUP BY 
+--         p.property_id,
+--         p.host_id,
+--         p.name,
+--         p.description,
+--         p.location,
+--         p.pricepernight,
+--         p.created_at,
+--         p.updated_at
+-- )
+-- SELECT 
+--     property_id,
+--     host_id,
+--     name,
+--     description,
+--     location,
+--     pricepernight,
+--     created_at,
+--     updated_at,
+--     total_bookings,
+--     RANK() OVER (ORDER BY total_bookings DESC) AS booking_rank
+-- FROM 
+--     BookingCounts;
+
 WITH BookingCounts AS (
     SELECT 
         p.property_id,
@@ -62,6 +103,6 @@ SELECT
     created_at,
     updated_at,
     total_bookings,
-    RANK() OVER (ORDER BY total_bookings DESC) AS booking_rank
+    ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS booking_rank
 FROM 
     BookingCounts;
